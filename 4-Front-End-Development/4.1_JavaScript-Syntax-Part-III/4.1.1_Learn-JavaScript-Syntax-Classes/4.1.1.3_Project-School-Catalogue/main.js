ef1855021ed97781d847f9f4801fb0ece4f85f84
Jeko92 <jkhachidze92@gmail.com>
2023-07-19 09:25:50 +0200

    JavaScript Syntax, Part III, Learn JavaScript Syntax:Classes, Lesson-Classes, Projects:Build a Library, School Catalogue

---
+++ 4-Front-End-Development/4.1_JavaScript-Syntax-Part-III/4.1.1_Learn-JavaScript-Syntax-Classes/4.1.1.3_Project-School-Catalogue/main.js
@@ -0,0 +1,102 @@
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set level(newLevel) {
+    this._level = newLevel;
+  }
+
+  set numberOfStudents(newNumOfStudents) {
+    return typeof newNumOfStudents === "number"
+      ? (this._numberOfStudents = newNumOfStudents)
+      : "Invalid input: numberOfStudents must be set to a Number.";
+  }
+
+  quickFacts() {
+    console.log(
+      `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`
+    );
+  }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    let randomTeacherNum = Math.floor(
+      Math.random() * (substituteTeachers.length - 1)
+    );
+    return substituteTeachers[randomTeacherNum];
+  }
+}
+
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, "primary", numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class MiddleSchool extends School {
+  constructor(name, numberOfStudents) {
+    // To use School parent constructor
+    super(name, "middle", numberOfStudents);
+  }
+}
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, "high", numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+
+  get sportsTeams() {
+    return `${this._name} School has following sports teams: ${this._sportsTeams}`;
+  }
+}
+
+const lorraineHansbury = new PrimarySchool(
+  "Lorraine Hansbury",
+  514,
+  "Students must be picked up by a parent, guardian, or a family member over the age of 13."
+);
+
+lorraineHansbury.quickFacts();
+const sub = School.pickSubstituteTeacher([
+  "Jamal Crawford",
+  "Lou Williams",
+  "J. R. Smith",
+  "James Harden",
+  "Jason Terry",
+  "Manu Ginobli",
+]);
+
+console.log(sub);
+
+const alSmith = new HighSchool("Al E. Smith", 415, [
+  "Baseball",
+  "Basketball",
+  "Volleyball",
+  "Track and Field",
+]);
+
+console.log(alSmith.sportsTeams);
