e8e045f7ae5933c08fcee307dad5f35a954ca0c5
Jeko92 <jkhachidze92@gmail.com>
2023-07-19 14:10:27 +0200

    JavaScript Syntax, Part III, Learn JavaScript Syntax:Modules, Article-Implementing Modules, Project:WorkAround Explorer

---
+++ 4-Front-End-Development/4.1_JavaScript-Syntax-Part-III/4.1.2_Learn-JavaScript-Syntax-Modules/4.1.2.1_Article-Implementing-Modules/secret-image/secret-image.js
@@ -0,0 +1,13 @@
+/* secret-images.js */
+import {
+  toggleHiddenElement,
+  changeToFunkyColor,
+} from "../modules/dom-functions.js";
+
+const buttonElement = document.getElementById("secret-button");
+const imgElement = document.getElementById("secret-img");
+
+buttonElement.addEventListener("click", () => {
+  toggleHiddenElement(imgElement);
+  changeToFunkyColor(buttonElement);
+});
