2af95b0d68c092c1496fc7b5153dac4fb04acc7e
Jeko92 <jkhachidze92@gmail.com>
2023-07-27 17:20:01 +0200

    JavaScript Testing, Learn TDD with Mocha, Lesson-Learn TDD with Mocha

--- 4-Front-End-Development/4.2_JavaScript-Testing/4.2.3_Learn-TDD-with-Mocha/4.2.3.1_Lesson-Learn-TDD-with-Mocha/index.js
+++ 4-Front-End-Development/4.2_JavaScript-Testing/4.2.3_Learn-TDD-with-Mocha/4.2.3.1_Lesson-Learn-TDD-with-Mocha/index.js
@@ -1,6 +1,39 @@
-const Calculate = {
-  sum(arr) {
+// 3/9 Red To Green I
+/* const Calculate = {
+  sum(inputArr) {
     return 6;
   },
+}; */
+
+// 6/9 Red To Green II
+/* const Calculate = {
+  sum(inputArray) {
+    let totalSum = 0;
+
+    const inputArrayLength = inputArray.length;
+    for (let i = 0; i < inputArrayLength; i++) {
+      totalSum += inputArray[i];
+    }
+
+    return totalSum;
+  },
+}; */
+
+// 7/9 Refactor II
+/* const Calculate = {
+  sum(inputArr) {
+    return inputArr.reduce((acc, curr) => acc + curr, 0);
+  },
+}; */
+
+// 8/9 Edge Case
+const Calculate = {
+  sum(inputArr) {
+    if (inputArr.length === 0) {
+      return 0;
+    }
+    return inputArr.reduce((acc, curr) => acc + curr, 0);
+  },
 };
+
 module.exports = Calculate;
