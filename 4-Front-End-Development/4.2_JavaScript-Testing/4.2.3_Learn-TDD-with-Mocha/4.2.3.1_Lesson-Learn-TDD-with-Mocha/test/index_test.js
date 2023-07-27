116f9d11236141710d731a92e8b7bb88070bafc9
Jeko92 <jkhachidze92@gmail.com>
2023-07-27 17:20:01 +0200

    JavaScript Testing, Learn TDD with Mocha, Lesson-Learn TDD with Mocha

--- 4-Front-End-Development/4.2_JavaScript-Testing/4.2.3_Learn-TDD-with-Mocha/4.2.3.1_Lesson-Learn-TDD-with-Mocha/test/index_test.js
+++ 4-Front-End-Development/4.2_JavaScript-Testing/4.2.3_Learn-TDD-with-Mocha/4.2.3.1_Lesson-Learn-TDD-with-Mocha/test/index_test.js
@@ -8,12 +8,66 @@ const Calculate = require("../index.js");
 }); */
 
 // 2/9 Getting Into The Red I
-describe("Calculate", () => {
+/* describe("Calculate", () => {
   describe(".sum", () => {
     it("returns the sum of an array of numbers", () => {
       // Code here
       const result = Calculate.sum([1, 2, 3]);
       assert.strictEqual(result, 6);
+    });
+  });
+}); */
+
+// 4/9 Refactor I
+/* describe("Calculate", () => {
+  describe(".sum", () => {
+    it("returns the sum of an array of numbers", () => {
+      // Code here
+      // Setup
+      const inputArr = [1, 2, 3];
+      const expectedNum = 6;
+      // Exercise
+      const result = Calculate.sum(inputArr);
+      // Verification
+      assert.strictEqual(result, expectedNum);
+    });
+  });
+}); */
+
+// 5/9 Getting into the Red II
+describe("Calculate", () => {
+  describe(".sum", () => {
+    it("returns the sum of an array of numbers", () => {
+      // Code here
+      // Setup
+      const inputArr = [1, 2, 3];
+      const expectedNum = 6;
+      // Exercise
+      const result = Calculate.sum(inputArr);
+      // Verification
+      assert.strictEqual(result, expectedNum);
+    });
+
+    it("returns the sum of a four-item list", () => {
+      // Code here
+      // Setup
+      const inputArr = [4, 5, 6, 7];
+      const expectedNum = 22;
+      // Exercise
+      const result = Calculate.sum(inputArr);
+      // Verification
+      assert.strictEqual(result, expectedNum);
+    });
+
+    it("returns zero for an empty array", () => {
+      // Code here
+      // Setup
+      const inputArr = [];
+      const expectedNum = 0;
+      // Exercise
+      const result = Calculate.sum(inputArr);
+      // Verification
+      assert.strictEqual(result, expectedNum);
     });
   });
 });
