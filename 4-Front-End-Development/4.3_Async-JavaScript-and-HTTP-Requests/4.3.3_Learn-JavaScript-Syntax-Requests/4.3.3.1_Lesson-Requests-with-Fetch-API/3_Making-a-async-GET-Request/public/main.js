ee5315f6e46c3c81b09fc5506b21630f4a9f0192
Jeko92 <jkhachidze92@gmail.com>
2023-08-01 10:02:21 +0200

    Async JavaScript and HTTP Requests, Learn JavaScript Syntax: Requests, Lesson-Requests with Fetch API, async GET and POST requests

---
+++ 4-Front-End-Development/4.3_Async-JavaScript-and-HTTP-Requests/4.3.3_Learn-JavaScript-Syntax-Requests/4.3.3.1_Lesson-Requests-with-Fetch-API/3_Making-a-async-GET-Request/public/main.js
@@ -0,0 +1,37 @@
+// Information to reach API
+const url = "https://api.datamuse.com/words?sl=";
+const queryParams = 'rel_jja=';
+
+// Selects page elements
+const inputField = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const responseField = document.querySelector("#responseField");
+
+// Asynchronous function
+const getSuggestions = async() => {
+  const wordQuery = inputField.value;
+  const endpoint = url +queryParams+ wordQuery;
+  try {
+    const response = await fetch(endpoint,{cache:"no-cache"});
+    if (response.ok) {
+      const jsonResponse= await response.json();
+      renderResponse(jsonResponse);
+    }
+    throw new Error("Request failed!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
