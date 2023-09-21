2deeb87b175a543398a99864a1d36c1b429b61bb
Jeko92 <jkhachidze92@gmail.com>
2023-09-21 23:44:25 +0200

    Redux, Core Redux API, Project-Codecademy Store

--- 4-Front-End-Development/4.7_Redux/4.7.2_Core-Redux-API/4.7.2.2_Lesson-Strategies-for-Complex-State/4.7.2.2.1-React-Reduc-App-Structure/favorite-recipes/src/app/store.js
+++ 4-Front-End-Development/4.7_Redux/4.7.2_Core-Redux-API/4.7.2.2_Lesson-Strategies-for-Complex-State/4.7.2.2.1-React-Reduc-App-Structure/favorite-recipes/src/app/store.js
@@ -2,11 +2,13 @@ import { legacy_createStore as createStore, combineReducers } from "redux";
 import { favoriteRecipesReducer } from "../features/favoriteRecipes/favoriteRecipesSlice.js";
 import { searchTermReducer } from "../features/searchTerm/searchTermSlice.js";
 import { allRecipesReducer } from "../features/allRecipes/allRecipesSlice.js";
+import { searchTermReducer } from "../features/searchTerm/searchTermSlice.js";
 
 export const store = createStore(
   combineReducers({
     favoriteRecipes: favoriteRecipesReducer,
     searchTerm: searchTermReducer,
     allRecipes: allRecipesReducer,
+    searchTerm: searchTermreducer,
   })
 );
