17d45fabcd5cb60232d0c72dc286567ac7c129aa
Jeko92 <jkhachidze92@gmail.com>
2023-09-26 09:53:18 +0200

    Redux, Async Actions with Middleware and Thunks, Project-Redux News Reader

---
+++ 4-Front-End-Development/4.7_Redux/4.7.4_Async-Actions-with-Middleware-and-Thunks/4.7.4.2_Project-Redux-News-Reader/redux-news-reader/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import store from "./app/store";
+import { Provider } from "react-redux";
+
+const { worker } = require("./mocks/browser");
+const root = createRoot(document.getElementById("root"));
+worker.start();
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
