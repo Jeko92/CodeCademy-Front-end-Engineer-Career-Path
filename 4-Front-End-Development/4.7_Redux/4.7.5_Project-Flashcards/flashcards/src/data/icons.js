c642b80cbafc28c2da6f7777f5bd321f35ddb442
Jeko92 <jkhachidze92@gmail.com>
2023-09-27 09:25:02 +0200

    Redux, Challenge Project:Flashcards

---
+++ 4-Front-End-Development/4.7_Redux/4.7.5_Project-Flashcards/flashcards/src/data/icons.js
@@ -0,0 +1,130 @@
+import bookIcon from "../assets/icons/book.svg";
+import balloonIcon from "../assets/icons/balloon.svg";
+import birdIcon from "../assets/icons/bird.svg";
+import calendarIcon from "../assets/icons/calendar.svg";
+import cloverIcon from "../assets/icons/clover.svg";
+import crayonsIcon from "../assets/icons/crayons.svg";
+import dataFlowIcon from "../assets/icons/data-flow.svg";
+import fenceIcon from "../assets/icons/fence.svg";
+import grillIcon from "../assets/icons/grill.svg";
+import handDrillIcon from "../assets/icons/hand-drill.svg";
+import hatIcon from "../assets/icons/hat.svg";
+import internetIcon from "../assets/icons/internet.svg";
+import ladybugIcon from "../assets/icons/ladybug.svg";
+import leavesIcon from "../assets/icons/leaves.svg";
+import medicineIcon from "../assets/icons/medicine.svg";
+import nestIcon from "../assets/icons/nest.svg";
+import shuttlecockIcon from "../assets/icons/shuttlecock.svg";
+import spadeIcon from "../assets/icons/spade.svg";
+import statisticsIcon from "../assets/icons/statistics.svg";
+import sunIcon from "../assets/icons/sun.svg";
+import treeIcon from "../assets/icons/tree.svg";
+
+export const BOOK_ICON = bookIcon;
+export const BALLOON_ICON = balloonIcon;
+export const BIRD_ICON = birdIcon;
+export const CALENDAR_ICON = calendarIcon;
+export const CLOVER_ICON = cloverIcon;
+export const CRAYONS_ICON = crayonsIcon;
+export const DATA_FLOW_ICON = dataFlowIcon;
+export const FENCE_ICON = fenceIcon;
+export const GRILL_ICON = grillIcon;
+export const HAND_DRILL_ICON = handDrillIcon;
+export const HAT_ICON = hatIcon;
+export const INTERNET_ICON = internetIcon;
+export const LADYBUG_ICON = ladybugIcon;
+export const LEAVES_ICON = leavesIcon;
+export const MEDICINE_ICON = medicineIcon;
+export const NEST_ICON = nestIcon;
+export const SHUTTLECOCK_ICON = shuttlecockIcon;
+export const SPADE_ICON = spadeIcon;
+export const STATISTICS_ICON = statisticsIcon;
+export const SUN_ICON = sunIcon;
+export const TREE_ICON = treeIcon;
+
+export const ALL_ICONS = [
+  {
+    url: BOOK_ICON,
+    name: "Book",
+  },
+  {
+    url: BALLOON_ICON,
+    name: "Balloon",
+  },
+  {
+    url: BIRD_ICON,
+    name: "Bird",
+  },
+  {
+    url: CALENDAR_ICON,
+    name: "Calendar",
+  },
+  {
+    url: CLOVER_ICON,
+    name: "Clover",
+  },
+  {
+    url: CRAYONS_ICON,
+    name: "Crayons",
+  },
+  {
+    url: DATA_FLOW_ICON,
+    name: "Data",
+  },
+  {
+    url: FENCE_ICON,
+    name: "Fence",
+  },
+  {
+    url: GRILL_ICON,
+    name: "Grill",
+  },
+  {
+    url: HAND_DRILL_ICON,
+    name: "Hand",
+  },
+  {
+    url: HAT_ICON,
+    name: "Hat",
+  },
+  {
+    url: INTERNET_ICON,
+    name: "Internet",
+  },
+  {
+    url: LADYBUG_ICON,
+    name: "Ladybug",
+  },
+  {
+    url: LEAVES_ICON,
+    name: "Leaves",
+  },
+  {
+    url: MEDICINE_ICON,
+    name: "Medicine",
+  },
+  {
+    url: NEST_ICON,
+    name: "Nest",
+  },
+  {
+    url: SHUTTLECOCK_ICON,
+    name: "Shuttlecock",
+  },
+  {
+    url: SPADE_ICON,
+    name: "Spade",
+  },
+  {
+    url: STATISTICS_ICON,
+    name: "Statistics",
+  },
+  {
+    url: SUN_ICON,
+    name: "Sun",
+  },
+  {
+    url: TREE_ICON,
+    name: "Tree",
+  },
+];
