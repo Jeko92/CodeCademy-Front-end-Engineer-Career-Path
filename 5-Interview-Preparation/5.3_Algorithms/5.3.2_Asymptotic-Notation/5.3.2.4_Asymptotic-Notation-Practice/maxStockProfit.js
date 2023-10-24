8e5b2f75a67667ee0e52c776f5aa5c0d41d721d2
Jeko92 <jkhachidze92@gmail.com>
2023-10-24 14:46:49 +0200

    Interview Preparation, Algorithms, Asymptotic Notation, Practice-Asymptotic Notation

--- 5-Interview-Preparation/5.3_Algorithms/5.3.2_Asymptotic-Notation/5.3.2.4_Asymptotic-Notation-Practice/maxStockProfit.js
+++ 5-Interview-Preparation/5.3_Algorithms/5.3.2_Asymptotic-Notation/5.3.2.4_Asymptotic-Notation-Practice/maxStockProfit.js
@@ -1,1 +1,32 @@
-function maxProfitDays(stockPrices) {}
+/*
+Maximize Stock Trading Profit
+Given the daily values of a stock, create a function called maxProfitDays() that, given a list of integers, will return the index value of the two elements that represent the day on which one
+should have bought a share and the day on which one should have sold a share based on the max profit.
+
+A list of integers will represent the stock price at the beginning or “opening bell” of each day for a week. You are required to buy and sell only once. You also must buy a stock before selling it.
+
+For example, given the list [17, 11, 60, 25, 150, 75, 31, 120], you can assume that index 0 represents day 0 and index 7 represents day 7. In this case, purchasing on day 1 and selling on day 4 would yield the most profit.
+If we were to call maxProfitDays([17, 11, 60, 25, 150, 75, 31, 120]), the function would return [1, 4].
+ */
+
+function maxProfitDays(stockPrices) {
+  let profit = 0;
+  let buyDay; //the day we buy
+  let sellDay; //the day we sell
+
+  // loop day to buy i from day 0 to last day in the array
+  for (let i = 0; i < stockPrices.length; i++) {
+    // loop day to sell from day i+1 to last day in the array
+    for (let j = i + 1; j < stockPrices.length; j++) {
+      //test whether the profit on the 2 days is higher than the maximum previous profit
+      if (stockPrices[j] - stockPrices[i] > profit) {
+        profit = stockPrices[j] - stockPrices[i];
+        buyDay = i;
+        sellDay = j;
+      }
+    }
+  }
+  return [buyDay, sellDay];
+}
+
+console.log(maxProfitDays([17, 11, 60, 25, 150, 75, 31, 120]));
--- 5-Interview-Preparation/5.3_Algorithms/5.3.2_Asymptotic-Notation/5.3.2.4_Asymptotic-Notation-Practice/stairmaster.js
+++ 5-Interview-Preparation/5.3_Algorithms/5.3.2_Asymptotic-Notation/5.3.2.4_Asymptotic-Notation-Practice/stairmaster.js
@@ -1,3 +1,42 @@
-function stairmaster(n) {}
+/*
+Stairmaster
+Write a function, stairmaster(n), that will compute the number of ways to climb a flight of n steps, taking 1, 2, or 3 steps at a time.
+
+Take the example of climbing n = 4 steps. There are seven different ways one can climb four stairs using 1, 2, or 3 steps at a time: [1,1,1,1] [2,1,1] [1,2,1] [1,1,2] [2,2] [1,3] [3,1].
+
+Make sure to find all permutations, not combinations, as the order matters. Climbing one step then two steps is different from climbing two steps then one step.
+ */
+
+function theseAddToSum(steps = [], sum) {
+  // Base cases
+  if (sum < 0) return []; // No Solution
+  if (sum === 0) return [[]]; // A Solution
+
+  let results = [];
+
+  if (steps.length < 1) return "error";
+
+  for (let i = 0; i < steps.length; i++) {
+    let cur = steps[i];
+    let remaining = sum - cur;
+    let c = theseAddToSum(steps, remaining);
+
+    for (let solution of c) {
+      solution.push(cur);
+      results.push(solution);
+    }
+  }
+
+  return results;
+}
+
+function stairmaster(n) {
+  // Base cases
+  if (n === 0) return 0;
+
+  let ways = theseAddToSum([1, 2, 3], n);
+  console.log(ways);
+  return ways.length;
+}
 
 console.log(stairmaster(4));
