2bd6ee2c2093616dfb984a741c1bbfdfe6e1037c
Jeko92 <jkhachidze92@gmail.com>
2023-10-30 08:26:42 +0100

    Interview Preparation, Search and Graph Search Algorithms, Binary Search and Search Trees, Lesson-Binary Search: Conceptural, Lesson-Binary Search: JavaScript, Practice-Binary Tree

---
+++ 5-Interview-Preparation/5.4_Search-and-Graph-Search-Algorithms/5.4.1_Binary-Search-and-Search-Trees/5.4.1.4_Binary-Search-Practice/1_BinarySearch.js
@@ -0,0 +1,27 @@
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length;
+
+  while (right > left) {
+    const indexToCheck = Math.floor((left + right) / 2);
+    const checking = arr[indexToCheck];
+    console.log(`indexToCheck equals: ${indexToCheck}`);
+
+    if (checking === target) {
+      return indexToCheck;
+    } // Add else if and else blocks:
+    else if (target > checking) {
+      left = indexToCheck + 1;
+    } else {
+      right = indexToCheck;
+    }
+  }
+  return null;
+};
+
+const searchable = [-1, 0, 3, 5, 9, 12];
+const target = 9;
+
+const targetIndex = binarySearch(searchable, target);
+
+console.log(`The target index is ${targetIndex}.`);
